cd875128c6aeb9615bcdfeff16f2676e9abc8f4d
Noah Pryor <noah@noahpryor.com>
2012-05-21 22:20:20 -0400

    nice startup funding in tables

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,7 +2,7 @@ class CompaniesController < ApplicationController
   # GET /companies
   # GET /companies.json
   def index
-    @companies = Company.all
+    @companies = Company.includes(:jobs).all
 
     respond_to do |format|
       format.html # index.html.erb
--- app/models/company.rb
+++ app/models/company.rb
@@ -5,6 +5,7 @@ class Company < ActiveRecord::Base
   attr_accessible :Crunchbase_url, :blog_url, :category, :company_hiring_text, :company_jobs_url, :company_name, :company_url, :description, :employees, :founded_month, :founded_year, :homepage_url, :money_raised, :name, :nocrunchbase, :overview, :tag_list, :twitter_name
   
   def funding_string
+    return nil if funding.nil?
     if funding > 1000000
       '$' + (funding / 1000000.0).to_s + 'M'
     elsif funding > 1000
