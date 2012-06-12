2d0d52a221c69909f3093d61923da9c71a0fbb9a
Noah Pryor <noah@noahpryor.com>
2012-06-12 18:48:49 -0400

    more graceful splashpage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,12 @@
 class HomeController < ApplicationController
   skip_before_filter :authenticate_user!
-  
+  def splash
+  end
+  def prelaunch 
+  end
   def index
     if !user_signed_in?
-      render :splash
+      render :prelaunch, :layout => false
     else
       @user = current_user
       @jobs = Job.all(:include => :company)
