7593dcecd54c4e6b219e697679cc1bcca7ec32d1
noahpryor <noah@noahpryor.com>
2012-09-24 00:17:25 -0400

    brargh

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module Realstartupjobs
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
@@ -52,7 +52,7 @@ module Realstartupjobs
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    config.assets.enabled = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
