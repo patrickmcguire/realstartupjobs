5a567ae38f4e3b2e9250662a8d869a4c7cafa5ab
noahpryor <noah@noahpryor.com>
2012-09-25 20:05:15 -0400

    launch page back

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,15 +5,15 @@ class HomeController < ApplicationController
   def prelaunch 
   end
   def index
-    if !user_signed_in?
-     #render :prelaunch, :layout => false
-    else
-      @user = current_user
-      @jobs = Job.all(:include => :company)
-      @saved_jobs = SavedJob.where(:user_id => current_user.id)
-      @saved_companies = SavedCompany.where(:user_id => current_user.id)
+
+     render :prelaunch, :layout => false
+   # else
+    #  @user = current_user
+     # @jobs = Job.all(:include => :company)
+      #@saved_jobs = SavedJob.where(:user_id => current_user.id)
+      #@saved_companies = SavedCompany.where(:user_id => current_user.id)
       
-      render :index
-    end
+    #  render :index
+   # end
   end
 end
